f70af0d71ce720b3ceb25b29435a18cedcec5cde
parva1474 <parva1474@gmail.com>
2026-08-06 19:05:40 +0330

    Update lessons.js

--- src/lessons.js
+++ src/lessons.js
@@ -4,12 +4,7 @@ import { lesson1 } from './lessons/lesson1.js';
 import { lesson2 } from './lessons/lesson2.js';
 import { lesson3 } from './lessons/lesson3.js';
 import { lesson4 } from './lessons/lesson4.js';
-import { lesson5 } from './lessons/lesson5.js';
-import { lesson6 } from './lessons/lesson6.js';
-import { lesson7 } from './lessons/lesson7.js';
-import { lesson8 } from './lessons/lesson8.js';
-import { lesson9 } from './lessons/lesson9.js';
-import { lesson10 } from './lessons/lesson10.js';
+;
 // (می‌توانید به همین ترتیب تا ۶۰ را یک‌بار اینجا اضافه کنید یا هر زمان که فایل lessonX را ساختید، فقط همان یک خط را اضافه کنید)
 
 // آرایه نهایی تمام درس‌ها
@@ -19,12 +14,7 @@ export const lessons = [
   lesson2,
   lesson3,
   lesson4,
-  lesson5,
-  lesson6,
-  lesson7,
-  lesson8,
-  lesson9,
-  lesson10,
+
   // اگر درس‌های ۱۱ به بعد را ساختید، به مرور اینجا اضافه کنید:
   // lesson11,
   // ... تا lesson60
